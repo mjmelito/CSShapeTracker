61cb07729ee91183fc2b7c07db4d9692d1317d20
Matt Melito <matt_melito@outlook.com>
2023-12-13 19:48:32 -0800

    Add updated Program and Model.Triangle code to add length parameters and console log triangle sides.

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -1,8 +1,19 @@
-    
+
+using System.Runtime.InteropServices;
+
 namespace ShapeTracker.Models
 {
     public class Triangle
     {
-        //code goes here
+        public int Side1;
+        public int Side2 = 4;
+        public int Side3;
+
+        public Triangle(int length1, int length2, int length3)
+        {
+            Side1 = length1;
+            Side2 = length2;
+            Side3 = length3;
+        }
     }
 }
--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using ShapeTracker.Models;
 
 namespace ShapeTracker
 {
@@ -6,8 +7,13 @@ namespace ShapeTracker
     {
         static void Main()
         {
-            Triangle testTriangle = new Triangle();
+            Triangle testTriangle = new Triangle(3, 4, 5);
             Console.WriteLine(testTriangle.GetType());
+
+
+            Console.WriteLine($"Side one of the triangle: {testTriangle.Side1}");
+            Console.WriteLine($"Side two of the triangle: {testTriangle.Side2}");
+            Console.WriteLine($"Side three of the triangle: {testTriangle.Side3}");
         }
     }
 }
