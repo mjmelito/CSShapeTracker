05b1a5ff8708869477d364aced842d432f10e2ae
Matt Melito <matt_melito@outlook.com>
2023-12-17 09:24:13 -0800

    Remove square functionality

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -17,14 +17,6 @@ namespace ShapeTracker
             Console.WriteLine("Want to know what type of triangle you have?");
             Console.WriteLine("Calculating...");
             Console.WriteLine($"Your triangle is: {testTriangle.CheckType()}.");
-
-            Square testSquare = new Square(3);
-            Console.WriteLine(testSquare.GetType());
-
-
-            Console.WriteLine($"Side one of the Square: {testSquare.Side1}");
-            Console.WriteLine($"Side two of the Square: {testSquare.Side2}");
-            Console.WriteLine($"Side three of the Square: {testSquare.Side3}");
         }
     }
 }
